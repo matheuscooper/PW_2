caf19a1dfc631e48f2fd3cfe7dd3aba7d615eba2
matheuscooper <oliveiracooper73@gmail.com>
2025-12-16 11:41:10 -0400

    fix: correct object

--- backend/src/useCases/updateUser/updaterUser.useCase.ts
+++ backend/src/useCases/updateUser/updaterUser.useCase.ts
@@ -20,19 +20,19 @@ export class UpdateUserUseCase {
     if (payload.email && payload.email !== user?.props.email) {
       const existingEmail = await this.userRepository.findByEmail(envs, payload.email);
       if (existingEmail) throw new Error("Email já está em uso");
-      user.props.email = payload.email;
+      user!.props.email = payload.email;
     }
 
     if (payload.nome) {
-      user.props.nome = payload.nome;
+      user!.props.nome = payload.nome;
     }
 
     if (payload.senha) {
       const hashed = await this.hasProvider.hash(payload.senha);
-      user.props.senha = hashed;
+      user!.props.senha = hashed;
     }
 
-    await this.userRepository.update(envs, userId, user.props);
+    await this.userRepository.update(envs, userId, user!.props);
 
     return user?.props;
   }
